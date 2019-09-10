2f74ff0dbe0d5a3c4bff568b206a011ed059ba3c
Loghan <loghan.r@gmail.com>
2019-09-10 11:37:16 +0200

    Connect NodeJS to MongoDB (localhost)

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+const Mongo = require('mongodb');
+const MongoClient = Mongo.MongoClient;
+const MongoObjectId = Mongo.ObjectID;
+
 const express = require('express');
 const app = express();
 const fs = require("fs");
@@ -10,22 +14,53 @@ const user = {
    }
 };
 
+var url = "mongodb://localhost:27017/myDB";
+
 app.get('/listUsers', function (req, res) {
-   fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
-      console.log(data);
-      res.end(data);
+   MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("co-work");
+      var query = {};
+      dbo.collection("users").find(query).toArray(function(err, result) {
+         if (err) throw err;
+         console.log(result);
+         res.end( JSON.stringify(result));
+         db.close();
+      });
    });
 });
 
+// app.get('/listUsers', function (req, res) {
+//    fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
+//       console.log(data);
+//       res.end(data);
+//    });
+// });
+
 app.get('/listUsers/:id', function (req, res) {
-   // First read existing users.
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-      const users = JSON.parse( data );
-      const user = users["user" + req.params.id];
-      console.log(user);
-      res.end( JSON.stringify(user));
+   MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("co-work");
+      var query = { _id : new MongoObjectId(req.params.id) };
+      console.log(query);
+      dbo.collection("users").find(query).toArray(function(err, result) {
+         if (err) throw err;
+         console.log(result);
+         res.end( JSON.stringify(result));
+         db.close();
+      });
    });
 });
+
+// app.get('/listUsers/:id', function (req, res) {
+//    // First read existing users.
+//    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+//       const users = JSON.parse( data );
+//       const user = users["user" + req.params.id];
+//       console.log(user);
+//       res.end( JSON.stringify(user));
+//    });
+// });
 
 app.post('/addUser', function (req, res) {
    // First read existing users.
