cd9e6dc34b7a387a10c48dcd30bcfe5a00ee259b
Loghan <loghan.r@gmail.com>
2019-09-27 18:55:07 +0200

    change file name for default start of heroku

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const MongoObjectId = Mongo.ObjectID;
 const databaseInfo = Object.freeze({"url":"mongodb+srv://loghan:Voitures97130@dvilcluster-ehelb.gcp.mongodb.net/test?retryWrites=true&w=majority", "name":"co-work"}); // "mongodb://dvilcluster-ehelb.gcp.mongodb.net:27017/"
 const serverInfo = Object.freeze({"host":"localhost", "port":process.env.PORT || 5000}); // "co-work-lrams.herokuapp.com"
 const collectionName = "users";
-const cors = require('cors');
+const Cors = require('cors');
 const BodyParser = require('body-parser');
-const path = require('path');
+const Path = require('path');
 const Express = require('express');
 const app = Express();
 
 app.use(BodyParser.json()); // support json encoded bodies
 app.use(BodyParser.urlencoded({ extended: true })); // support encoded bodies
-app.use(cors({origin: 'http://localhost:4200'})); // 109.31.193.30
-app.use(Express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
+app.use(Cors({origin: 'http://localhost:4200'})); // 109.31.193.30
+app.use(Express.static(Path.join(__dirname, 'public')));
+app.set('views', Path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //app.get('/', (req, res) => res.render('pages/index'))
